b26c3b3e4f3d909a5d98fa006e575195289d8d41
Emilio Ceccotti <emilio.ceccotti95@gmail.com>
2020-11-18 17:01:53 +0100

    Update plot_fits.py

--- scripts/plot_fits.py
+++ scripts/plot_fits.py
@@ -6,17 +6,19 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 from astropy.wcs import WCS
 
-o = optparse.OptionParser()
-o.set_usage('plot_fits.py [options] *.fits')
-o.add_option('--size', dest='size', default="8,6", type="str", help='Size of the output image (default: 8,6)')
-o.add_option('--vmin', dest='vmin', default=None, type="float", help='Minimum value of the colormap (default: data minimum)')
-o.add_option('--vmax', dest='vmax', default=None, type="float", help='Maximum value of the colormap (default: data maximum)')
-o.add_option('--cmap', dest='cmap', default="jet", type="str", help='Colormap (default: jet)')
-o.add_option('--cbar_label', dest='cbar_label', default=None, type="str", help='Label of the color bar (default: BUNIT from header)')
-o.add_option('--title', dest='title', default=None, type="str", help='Title of the image (default: fits name)')
-o.add_option('--pdf', dest='save_pdf', default=False, action='store_true', help='Save image as pdf')
-o.add_option('--png', dest='save_png', default=False, action='store_true', help='Save image as png')
-o.add_option('--name', dest='outfile', default=None, type="str", help='Output image name with no extension (default: fits name)')
+o = optparse.OptionParser(usage='%prog [options] *.fits')
+o.add_option('--size', dest='size', nargs=2, default=(8, 6), type="float", help='size of the output image (default: 8 6)')
+o.add_option('--vmin', dest='vmin', default=None, type="float", help='minimum value of the colormap (default: data minimum)')
+o.add_option('--vmax', dest='vmax', default=None, type="float", help='maximum value of the colormap (default: data maximum)')
+o.add_option('--cmap', dest='cmap', default="jet", type="str", help='colormap (default: jet)')
+o.add_option('--cbar_label', dest='cbar_label', default=None, type="str", help='label of the color bar (default: BUNIT from header)')
+o.add_option('--gridc', dest='gridc', default="silver", type="str", help='coordinates grid color (default: silver)')
+o.add_option('--title', dest='title', default=None, type="str", help='title of the image (default: fits name)')
+g = optparse.OptionGroup(o, 'Saving Options')
+g.add_option('--pdf', dest='save_pdf', default=False, action='store_true', help='save image as pdf')
+g.add_option('--png', dest='save_png', default=False, action='store_true', help='save image as png')
+g.add_option('--name', dest='outfile', default=None, type="str", help='output image name with no extension (default: fits name)')
+o.add_option_group(g)
 opts,args = o.parse_args(sys.argv[1:])
 
 fitsname = args[0]
@@ -25,13 +27,10 @@ hdr = hdul[0].header
 data = hdul[0].data[0,0,:,:]
 wcs = WCS(hdr,naxis=2)
 
-imsize = np.array(opts.size.split(','), dtype=np.float64)
-fig = plt.figure(figsize=(imsize[0],imsize[1]))
+fig = plt.figure(figsize=opts.size)
 ax = fig.add_subplot(1,1,1, projection=wcs)
 ra = ax.coords[0]
 dec = ax.coords[1]
-#ra.set_major_formatter('hh:mm:ss')
-#dec.set_major_formatter('dd:mm:ss')
 
 if opts.vmin == None: opts.vmin = np.min(data)
 if opts.vmax == None: opts.vmax = np.max(data)
@@ -41,7 +40,7 @@ plt.imshow(data, origin='lower', cmap=opts.cmap,
 	aspect='auto', interpolation='none', 
 	vmin=opts.vmin, vmax=opts.vmax)
 plt.colorbar().set_label(opts.cbar_label, rotation=90)
-ax.coords.grid(color='silver', ls='solid', alpha=0.5)
+ax.coords.grid(color=opts.gridc, ls='solid', lw=0.50)
 plt.xlabel('Right Ascension (J2000)')
 plt.ylabel('Declination (J2000)')
 
