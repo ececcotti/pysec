883cef1d35f7db5d53cf8dc9e4ae03113edb05c5
Emilio Ceccotti <emilio.ceccotti95@gmail.com>
2021-03-18 15:36:42 +0100

    Update h5sol_utils.py with baselineGains function

--- scripts/h5sol_utils.py
+++ scripts/h5sol_utils.py
@@ -22,59 +22,66 @@ def ant2coord(ants):
 
     return ant_coord
 
-def antennaGains(amp, phase, avg_time=False, avg_frequency=False, avg_antenna=False, avg_polarization=False):
+def antennaGains(amp, phase, avg_time=False, avg_frequency=False, avg_polarization=False):
 
+    ant_axis = 2
+    avg_antenna = False
     avg = np.array([avg_time, avg_frequency, avg_antenna, avg_polarization])
 
     if np.any(avg) == False:
-        return amp[:] * np.exp(1j * phase[:])
+        return amp[:] * np.exp(1j * phase[:]), ant_axis
     else: 
-        wTrue = tuple(np.where(avg==True)[0])
-        #return np.nanmean(amp, axis=wTrue) * np.exp(1j * np.nanmean(phase, axis=wTrue))
-        return np.nanmean(amp[:] * np.exp(1j * phase[:]), axis=wTrue)
+        wTrue = np.where(avg==True)[0]
+        if wTrue.size == 1:
+            if wTrue[0] == 0 or wTrue[0] == 1: ant_axis =1
+        elif wTrue.size > 1:
+            if wTrue[0] == 0 and wTrue[1] == 1: ant_axis = 0
+        return np.nanmean(amp, axis=tuple(wTrue)) * np.exp(-1j * np.nanmean(phase, axis=tuple(wTrue))), ant_axis
+        #return np.nanmean(amp[:] * np.exp(1j * phase[:]), axis=wTrue)
 
-def baselineGains(ant_gains, ant_coord, ant_axis, avg_time=False, avg_frequency=False, avg_antenna=False, avg_polarization=False, nolofar=False):
+def baselineGains(ant_gains, ant_coord, ant_axis, avg_time=False, avg_frequency=False, avg_polarization=False, ants=None):
 
     nant = ant_coord.shape[0]
 
     bll = np.zeros(int(nant*(nant-1)/2), dtype=np.float64)
-    #blgain = np.zeros((int(nant*(nant-1)/2), npol), dtype=np.complex128)
 
+    shape_blgain = []
+    for i in range(len(ant_gains.shape)):
+        if i == ant_axis: # substitute the antenna axis with the baseline axis N(N-1)/2
+            shape_blgain.append(int(nant*(nant-1)/2.))
+        else: shape_blgain.append(ant_gains.shape[i])
+    
+    bl_gains = np.zeros(shape_blgain, dtype=np.complex128)
+
+    avg_antenna = False
     avg = np.array([avg_time, avg_frequency, avg_antenna, avg_polarization])
 
+    w_cc = []; w_cr = []; w_rr = []
     ant_count = 0
     bl_count = 0
-    for i in range(nant):
-        for j in range(ant_count,nant):
+    slc = [slice(None)] * bl_gains.ndim
+    for i in range(ant_gains.shape[ant_axis]):
+        for j in range(ant_count,ant_gains.shape[ant_axis]):
             if i != j: 
                 bll[bl_count] = np.sqrt(np.sum(np.square(ant_coord[i] - ant_coord[j]))) 
                 if np.any(avg) == False:
-                    blgain.append(again[i,:] * np.conj(again[j,:])
+                    slc[ant_axis] = bl_count
+                    bl_gains[tuple(slc)] = ant_gains.take(i, axis=ant_axis) * ant_gains.take(j, axis=ant_axis).conj()
+
+                st1 = ants[i][0].decode('utf-8')[0]
+                st2 = ants[j][0].decode('utf-8')[0]
+                if st1 == 'C' and st2 == 'C': w_cc.append(bl_count)
+                elif st1 == 'R' and st2 == 'R': w_rr.append(bl_count)
+                else: w_cr.append(bl_count)
+        
                 bl_count += 1
-        ant_count +=1
-#    w_cc = []; w_cr = []; w_rr = []
-#    acount = 0
-#    bcount = 0
-#    for i in range(nant):
-#        for j in range(acount,nant):
-#            if i != j: 
-#                bll[bcount] = np.sqrt(np.sum(np.square(ants_coord[i] - ants_coord[j]))) 
-#                #blgain[bcount, :] = again[i,:] * np.conj(again[j,:])
-#                blgain[bcount, :] = np.nanmean(again[:,:,i,:] * np.conj(again[:,:,j,:]), axis=(0,1))
-
-#                # index of CS-CS, RS-RS, CS-RS baselines
-#                st1 = ants[i][0].decode('utf-8')[0]
-#                st2 = ants[j][0].decode('utf-8')[0]
-#                if st1 == 'C' and st2 == 'C': w_cc.append(bcount)
-#                elif st1 == 'R' and st2 == 'R': w_rr.append(bcount)
-#                else: w_cr.append(bcount)
-
-#                bcount += 1
-#        acount += 1
+        ant_count += 1
 
     w_cc = np.asarray(w_cc)
     w_rr = np.asarray(w_rr)
     w_cr = np.asarray(w_cr)
+
+    return bll, bl_gains, [w_cc, w_rr, w_cr]
 
 
 if __name__=="__main__":
@@ -82,7 +89,8 @@ if __name__=="__main__":
     o = optparse.OptionParser(usage='%prog [options] *.h5')
     o.add_option('--nsol', nargs=2, dest='nsol', default=('000','000'), type="str", help='Numbers of solution set and subtable (default: 000 000)')
     o.add_option('--frqu', dest='frqu', default='MHz', type="str", help='Frequency unit for plot (default: MHz)')
-    o.add_option('--nolofar', dest='nolofar', default=True, action='store_false', help='Disable functionalities which work only with LOFAR data')
+    o.add_option('--ag_avg', nargs=3, dest='ag_avg', default=(1,1,0), type="int", 
+        help='Average antenna gains in time, frequency and/or polarization. Use 0 (False) and 1 (True) to average one or more axis, which are given in the previous order (default: 1 1 0)')
 
     opts,args = o.parse_args(sys.argv[1:])
 
@@ -95,59 +103,25 @@ if __name__=="__main__":
     frqu = freqUnit(opts.frqu)
 
     frq_range = [np.min(sols['amplitude' + opts.nsol[1]]['freq'])/frqu, np.max(sols['amplitude' + opts.nsol[1]]['freq'])/frqu]
-    nt = amp.shape[0]
-    nf = amp.shape[1]
-    nant = amp.shape[2]
-    npol = amp.shape[3]
+    nt, nf, nant, npol = [amp.shape[i] for i in range(amp.ndim)]
 
-    ant_gains = antennaGains(amp,phase)
+    # antenna gains
+    ant_gains, ant_axis = antennaGains(amp, phase, avg_time=opts.ag_avg[0], avg_frequency=opts.ag_avg[1], avg_polarization=opts.ag_avg[2])
     ant_coord = ant2coord(sols['antenna'])
+    
+    # baseline gains
+    bll, bl_gains, w = baselineGains(ant_gains, ant_coord, ant_axis=ant_axis, ants=sols['antenna'])
 
     """
-    ants = sols['antenna']
-    ants_coord = []
-    for i in range(ants.shape[0]):
-        ants_coord.append(ants[i][1])
-
-    ants_coord = np.array(ants_coord)
-    ants_coord -= ants_coord[0]
-
-    bll = np.zeros(int(nant*(nant-1)/2), dtype=np.float64)
-    blgain = np.zeros((int(nant*(nant-1)/2), npol), dtype=np.complex128)
-
-    w_cc = []; w_cr = []; w_rr = []
-    acount = 0
-    bcount = 0
-    for i in range(nant):
-        for j in range(acount,nant):
-            if i != j: 
-                bll[bcount] = np.sqrt(np.sum(np.square(ants_coord[i] - ants_coord[j]))) 
-                #blgain[bcount, :] = again[i,:] * np.conj(again[j,:])
-                blgain[bcount, :] = np.nanmean(again[:,:,i,:] * np.conj(again[:,:,j,:]), axis=(0,1))
-
-                # index of CS-CS, RS-RS, CS-RS baselines
-                st1 = ants[i][0].decode('utf-8')[0]
-                st2 = ants[j][0].decode('utf-8')[0]
-                if st1 == 'C' and st2 == 'C': w_cc.append(bcount)
-                elif st1 == 'R' and st2 == 'R': w_rr.append(bcount)
-                else: w_cr.append(bcount)
-
-                bcount += 1
-        acount += 1
-
-    w_cc = np.asarray(w_cc)
-    w_rr = np.asarray(w_rr)
-    w_cr = np.asarray(w_cr)
-    
     fitdeg = 4
     fitp = np.polynomial.polynomial.Polynomial.fit(x=bll, y=np.abs(blgain[:,0]), deg=fitdeg)
     xfit = np.arange(np.min(bll), np.max(bll), np.max(bll)/100)
 
-
+    """
     plt.figure(figsize=(8,5))
-    plt.plot(bll[w_cc], np.abs(blgain[w_cc,0]), '.', mew=0, label='CS-CS baseline', alpha=0.75)
-    plt.plot(bll[w_rr], np.abs(blgain[w_rr,0]), '.', mew=0, label='RS-RS baseline', alpha=0.75)
-    plt.plot(bll[w_cr], np.abs(blgain[w_cr,0]), '.', mew=0, label='CS-RS baseline', alpha=0.75)
+    plt.plot(bll[w[0]], np.abs(bl_gains[w[0],0]), '.', mew=0, label='CS-CS baseline', alpha=0.75)
+    plt.plot(bll[w[1]], np.abs(bl_gains[w[1],0]), '.', mew=0, label='RS-RS baseline', alpha=0.75)
+    plt.plot(bll[w[2]], np.abs(bl_gains[w[2],0]), '.', mew=0, label='CS-RS baseline', alpha=0.75)
     #plt.plot(xfit, fitp(xfit), '-', color='C3', label='polyfit deg=4')
     plt.axvline(5000, ls='--', color='C3')
     plt.xlabel('Baseline length (m)')
@@ -156,6 +130,7 @@ if __name__=="__main__":
     plt.grid(color='silver')
     plt.legend()
     plt.tight_layout()
-    plt.savefig('bslgainsss_%i-%iMHz.pdf'%(frq_range[0], frq_range[1]))
+    plt.show()
+    #plt.savefig('bslgainsss_%i-%iMHz.pdf'%(frq_range[0], frq_range[1]))
     plt.close()
-    """
+    
