dfd3d62fc2d3d9e857f494fd22d5879c0864f886
ececcotti <emilio.ceccotti95@gmail.com>
2021-03-30 17:49:48 +0200

    Add some plotting options and enable directions for DD solutions

--- scripts/h5sol_utils.py
+++ scripts/h5sol_utils.py
@@ -30,7 +30,7 @@ def antennaGains(amp, phase, avg_time=False, avg_frequency=False, avg_polarizati
     avg = np.array([avg_time, avg_frequency, avg_antenna, avg_polarization])
 
     if np.any(avg) == False:
-        return amp[:] * np.exp(1j * phase[:]), ant_axis
+        return amp * np.exp(1j * phase), ant_axis
     else: 
         wTrue = np.where(avg==True)[0]
         if wTrue.size == 1:
@@ -84,14 +84,61 @@ def baselineGains(ant_gains, ant_coord, ant_axis, avg_time=False, avg_frequency=
 
     return bll, bl_gains, [w_cc, w_rr, w_cr]
 
+def plotBaselineGains(bl_length, bl_gains, f_range, pol=0, w=None, plot_size=(8,5), save_pdf=False, save_png=False, direction=None):
+
+    if pol not in [0,1,2,3]: raise ValueError("Invalid polarization (only 0, 1, 2, 3 accepted)")
+
+    title = 'solutions at %.1f-%.1f MHz' % (f_range[0], f_range[1])
+    outfile = 'bslgains_%i-%iMHz' % (f_range[0], f_range[1])
+
+    if direction != None:
+        title += ', dir %s' % direction
+        outfile += '-%s' % direction
+
+    plt.figure(figsize=plot_size)
+    if w != None:
+        plt.plot(bll[w[0]], np.abs(bl_gains[w[0],pol]), '.', mew=0, label='CS-CS baseline', alpha=0.75)
+        plt.plot(bll[w[1]], np.abs(bl_gains[w[1],pol]), '.', mew=0, label='RS-RS baseline', alpha=0.75)
+        plt.plot(bll[w[2]], np.abs(bl_gains[w[2],pol]), '.', mew=0, label='CS-RS baseline', alpha=0.75)
+    else: 
+        plt.plot(bll, np.abs(bl_gains[:,pol]), '.', mew=0, label='CS-CS baseline', alpha=0.75)
+
+    plt.axvline(5000, ls='--', color='C3')
+    plt.xlabel('Baseline length (m)')
+    plt.ylabel('Gain amplitude')
+    plt.grid(color='silver')
+    plt.title(title)
+    plt.legend()
+    plt.tight_layout()
+
+    if save_pdf:
+        plt.savefig(outfile + '.pdf')
+    if save_png:
+        plt.savefig(outfile + '.png', dpi=200)
+
+    plt.show()
+    plt.close()
+
 
 if __name__=="__main__":
 
     o = optparse.OptionParser(usage='%prog [options] *.h5')
     o.add_option('--nsol', nargs=2, dest='nsol', default=('000','000'), type="str", help='Numbers of solution set and subtable (default: 000 000)')
-    o.add_option('--frqu', dest='frqu', default='MHz', type="str", help='Frequency unit for plot (default: MHz)')
+    o.add_option('--ddsol', dest='ddsol', default=False, action='store_true', help="Enable selection of direction subtables for direction-dependent solutions")
+    o.add_option('--dir', dest='dir', default=0, type="int", help='Direction subtable (default: 0)')
     o.add_option('--ag_avg', nargs=3, dest='ag_avg', default=(1,1,0), type="int", 
         help='Average antenna gains in time, frequency and/or polarization. Use 0 (False) and 1 (True) to average one or more axis, which are given in the previous order (default: 1 1 0)')
+    o.add_option('--bg_avg', nargs=3, dest='bg_avg', default=(0,0,0), type="int", 
+        help='Average baseline gains in time, frequency and/or polarization. Use 0 (False) and 1 (True) to average one or more axis, which are given in the previous order (default: 0 0 0)')
+    
+    g = optparse.OptionGroup(o, 'Plotting Options')
+    g.add_option('--frqu', dest='frqu', default='MHz', type="str", help='Frequency unit for plot (default: MHz)')
+    g.add_option('--ppol', dest='ppol', default=0, type="int", help='Select polarization to plot: 0=xx, 1=xy, 2=yx, 3=yy (default: 0)')
+    g.add_option('--plotb', dest='plotb', default=False, action='store_true', help="Plot baseline gains")
+    g.add_option('--psize', nargs=2, dest='psize', default=(8,5), type="int", help='Size (x y) of the selected plot (default: 8 5)')
+    g.add_option('--pdf', dest='save_pdf', default=False, action='store_true', help='Save plot as pdf')
+    g.add_option('--png', dest='save_png', default=False, action='store_true', help='Save plot as png')
+    o.add_option_group(g)
 
     opts,args = o.parse_args(sys.argv[1:])
 
@@ -104,38 +151,32 @@ if __name__=="__main__":
     frqu = freqUnit(opts.frqu)
 
     frq_range = [np.min(sols['amplitude' + opts.nsol[1]]['freq'])/frqu, np.max(sols['amplitude' + opts.nsol[1]]['freq'])/frqu]
-    nt, nf, nant, npol = [amp.shape[i] for i in range(amp.ndim)]
+    
+    slc_sol = [slice(None)] * amp.ndim
+    if opts.ddsol: 
+        nt, nf, nant, ndir, npol = [amp.shape[i] for i in range(amp.ndim)]
+        slc_sol[3] = opts.dir
+        dir_name = sols['amplitude' + opts.nsol[1]]['dir'][opts.dir].decode('utf-8')[1:-1]
+    else: 
+        nt, nf, nant, npol = [amp.shape[i] for i in range(amp.ndim)]
+        dir_name = None
 
     # antenna gains
-    #ant_gains, ant_axis = antennaGains(amp, phase, avg_time=opts.ag_avg[0], avg_frequency=opts.ag_avg[1], avg_polarization=opts.ag_avg[2])
-    #ant_coord = ant2coord(sols['antenna'])
+    print ('Generate antenna gains')
+    ant_gains, ant_axis = antennaGains(amp[tuple(slc_sol)], phase[tuple(slc_sol)], 
+            avg_time=opts.ag_avg[0], avg_frequency=opts.ag_avg[1], avg_polarization=opts.ag_avg[2])
+    ant_coord = ant2coord(sols['antenna'])
     
     # baseline gains
-    #bll, bl_gains, w = baselineGains(ant_gains, ant_coord, ant_axis=ant_axis, ants=sols['antenna'])
+    print ('Generate baseline gains')
+    bll, bl_gains, w = baselineGains(ant_gains, ant_coord, ant_axis=ant_axis, 
+        avg_time=opts.bg_avg[0], avg_frequency=opts.bg_avg[1], avg_polarization=opts.bg_avg[2], ants=sols['antenna'])
 
-    """
-    fitdeg = 4
-    fitp = np.polynomial.polynomial.Polynomial.fit(x=bll, y=np.abs(blgain[:,0]), deg=fitdeg)
-    xfit = np.arange(np.min(bll), np.max(bll), np.max(bll)/100)
-
+    # polt baseline gains
+    print ('Plot')
+    plotBaselineGains(bll, bl_gains, frq_range, pol=opts.ppol, w=w, plot_size=opts.psize, save_pdf=opts.save_pdf, save_png=opts.save_png, direction=dir_name)
     
-    plt.figure(figsize=(8,5))
-    plt.plot(bll[w[0]], np.abs(bl_gains[w[0],0]), '.', mew=0, label='CS-CS baseline', alpha=0.75)
-    plt.plot(bll[w[1]], np.abs(bl_gains[w[1],0]), '.', mew=0, label='RS-RS baseline', alpha=0.75)
-    plt.plot(bll[w[2]], np.abs(bl_gains[w[2],0]), '.', mew=0, label='CS-RS baseline', alpha=0.75)
-    #plt.plot(xfit, fitp(xfit), '-', color='C3', label='polyfit deg=4')
-    plt.axvline(5000, ls='--', color='C3')
-    plt.xlabel('Baseline length (m)')
-    plt.ylabel('Gain amplitude')
-    plt.title('solutions at %.1f-%.1f MHz' % (frq_range[0], frq_range[1]))
-    plt.grid(color='silver')
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
-    #plt.savefig('bslgainsss_%i-%iMHz.pdf'%(frq_range[0], frq_range[1]))
-    plt.close()
     """
-
     clock = sols['clock' + opts.nsol[1]]['val'][:,:,0]
     time = (sols['amplitude' + opts.nsol[1]]['time'][:]-sols['amplitude' + opts.nsol[1]]['time'][0])/3600.
     for i in range(clock.shape[1]):
@@ -163,3 +204,4 @@ if __name__=="__main__":
     plt.title('clock delay at %.1f-%.1f MHz' % (frq_range[0], frq_range[1]))
     plt.savefig('clock_%i-%iMHz.pdf'%(frq_range[0], frq_range[1]))
     plt.close()
+    """
